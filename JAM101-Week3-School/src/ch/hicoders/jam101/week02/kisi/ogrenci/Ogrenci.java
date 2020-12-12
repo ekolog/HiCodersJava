9fbe1aa176c1b5817416e49a8d300ee662479bd7
ekolog <ekolog055@gmail.com>
2020-12-12 15:00:37 +0100

    assignment uploaded

--- JAM101-Week3-School/src/ch/hicoders/jam101/week02/kisi/ogrenci/Ogrenci.java
+++ JAM101-Week3-School/src/ch/hicoders/jam101/week02/kisi/ogrenci/Ogrenci.java
@@ -1,11 +1,12 @@
 package ch.hicoders.jam101.week02.kisi.ogrenci;
 
 public class Ogrenci {
-    public String ogrenciIsmi;
+    public int ogrenciIsmi;
     public int almancaNotu;
     public int matematikNotu;
 
-    public Ogrenci(String pOgrenciIsmi, int pAlmancaNotu, int pMatematikNotu){
+
+    public Ogrenci(int pOgrenciIsmi, int pAlmancaNotu,int pMatematikNotu ){
         this.ogrenciIsmi = pOgrenciIsmi;
         this.almancaNotu = pAlmancaNotu;
         this.matematikNotu = pMatematikNotu;
